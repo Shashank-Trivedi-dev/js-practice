158adde80b1658a50eaec021e26e90d2b26704c4
CYBER <shashanktrivedidev@gmail.com>
2024-07-22 02:08:57 +0530

    3

--- main.js
+++ main.js
@@ -21,7 +21,38 @@ if (sex == "male") {
 } else {
   console.log("hello " + name);
 }
-
+// task 4
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
+
+// task 5
+let z = 0;
+for (i = 0; i <= 1000; i++) {
+  z = z + i;
+}
+console.log(z);
+
+// task 6
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 999];
+for (i = 0; i < array.length; i++) {
+  if (array[i] % 2 == 0) {
+    console.log(array[i]);
+  }
+}
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 == 0) {
+//     console.log(array[i]);
+//   }
+// }
+
+// task 7
+let big = array[0];
+for (i = 0; i < array.length; i++) {
+  if (array[i] > big) {
+    big = array[i];
+  }
+}
+console.log(big);
